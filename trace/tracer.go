80ca7dcf7c1558bfec2902e48506b70d05eaa610
aikuma0130 <ktaka0130@gmail.com>
2018-09-05 20:35:35 +0900

    Add implimentation in Trace

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,6 +1,9 @@
 package trace
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 type Tracer interface {
 	Trace(...interface{})
@@ -10,7 +13,10 @@ type tracer struct {
 	out io.Writer
 }
 
-func (t *tracer) Trace(a ...interface{}) {}
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
 
 func New(w io.Writer) Tracer {
 	return &tracer{out: w}
