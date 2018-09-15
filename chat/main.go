1657a9ebe4b7d2628ce878bf9b7f6e5b52d7dc4a
Dakie <ktaka0130@gmail.com>
2018-09-15 16:31:10 +0900

    Make template to get data instead of http request

--- chat/main.go
+++ chat/main.go
@@ -10,6 +10,7 @@ import (
 	"sync"
 
 	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
 
 	"github.com/aikuma0130/goWeb/trace"
 	//"github.com/stretchr/gomniauth/providers/facebook"
@@ -27,7 +28,16 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.temp1 = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.temp1.Execute(w, r)
+
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.temp1.Execute(w, data)
 }
 
 func main() {
