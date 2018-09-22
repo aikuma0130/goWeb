0c45ace612057a76de88b19ffe312b472db9bf5a
Dakie <ktaka0130@gmail.com>
2018-09-22 21:08:05 +0900

    Add tests for Avatar

---
+++ chat/avatar_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestAuthAvatar(t *testing.T) {
+	var authAvatar AuthAvatar
+	client := new(client)
+	url, err := authAvatar.GetAvatarURL(client)
+	if err != ErrNoAvatarURL {
+		t.Error("値が存在しない場合、AuthAvatar.GetAvatarURLは" + "ErrNoAvatarURL を返すべきです ")
+	}
+	// 値をセットします
+	testUrl := "http://url-to-avatar/"
+	client.userData = map[string]interface{}{"avatar_url": testUrl}
+	url, err = authAvatar.GetAvatarURL(client)
+	if err != nil {
+		t.Error("値が存在する場合、AuthAvatar.GetAvatarURLは" + " エラーを返すべきではありません ")
+	} else {
+		if url != testUrl {
+			t.Error("AuthAvatar.GetAvatarURL は正しい URL を返すべきです ")
+		}
+	}
+}
