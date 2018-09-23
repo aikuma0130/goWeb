c89c7edd5dba3b53d9b0033a5e37bf5535fc6444
Dakie <ktaka0130@gmail.com>
2018-09-23 20:22:10 +0900

    Add GetAvatarURL for passing test

--- chat/avatar.go
+++ chat/avatar.go
@@ -9,3 +9,16 @@ var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得でき�
 type Avatar interface {
 	GetAvatarURL(c *client) (string, error)
 }
+
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
