e61151e979bdd0e1557ff2dcb269bea6b3f8b6cd
Dakie <ktaka0130@gmail.com>
2018-09-24 23:28:58 +0900

    Add comments for Avatar

--- chat/avatar.go
+++ chat/avatar.go
@@ -4,9 +4,15 @@ import (
 	"errors"
 )
 
+// ErrNoAvatarはAvatarインスタンスがアバターのURLを返すことができない
+// 場合に発生するエラーです。
 var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得できません。")
 
+// Avatarはユーザーのプロフィール画像を表す型です。
 type Avatar interface {
+	// GetAvatarURLは指定されたクライアントのアバターのURLを返します。
+	// 問題が発生した場合にはエラーを返します。特に、URLを取得できなかった
+	// 場合にはErrNoAvatarURLを返します。
 	GetAvatarURL(c *client) (string, error)
 }
 
