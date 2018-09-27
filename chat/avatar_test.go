6b3c4c9af7ce369a4ad7d9103d16960281927008
aikuma0130 <ktaka0130@gmail.com>
2018-09-27 18:47:54 +0900

    Change tests for GravatarAvatar to use user_id instead of email

--- chat/avatar_test.go
+++ chat/avatar_test.go
@@ -25,7 +25,9 @@ func TestAuthAvatar(t *testing.T) {
 func TestGravatarAvatar(t *testing.T) {
 	var gravatarAvatar GravatarAvatar
 	client := new(client)
-	client.userData = map[string]interface{}{"email": "MyEmailAddress@example.com"}
+	client.userData = map[string]interface{}{
+		"user_id": "0bc83cb571cd1c50ba6f3e8a78ef1346",
+	}
 	testUrl := "//www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346"
 	url, err := gravatarAvatar.GetAvatarURL(client)
 	if err != nil {
