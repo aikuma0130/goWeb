87c28edbde18b58e13330455d4eb64090596a4df
Dakie <ktaka0130@gmail.com>
2018-10-01 23:13:12 +0900

    Register uploaderHandler for /uploader

--- chat/main.go
+++ chat/main.go
@@ -76,6 +76,7 @@ func main() {
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	http.Handle("/upload", &templateHandler{filename: "upload.html"})
+	http.HandleFunc("/uploader", uploaderHandler)
 
 	go r.run()
 
