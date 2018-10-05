e5417dd1f4968cffc34266bf88806accb383816a
Dakie <ktaka0130@gmail.com>
2018-10-05 18:53:14 +0900

    Use ChatUser instead of client for GetAvatarURL

--- chat/avatar.go
+++ chat/avatar.go
@@ -23,44 +23,34 @@ type AuthAvatar struct{}
 
 var UseAuthAvatar AuthAvatar
 
-func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
-	if url, ok := c.userData["avatar_url"]; ok {
-		if urlStr, ok := url.(string); ok {
-			return urlStr, nil
-		}
+func (_ AuthAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	url := u.AvatarURL()
+	if url != "" {
+		return "", ErrNoAvatarURL
 	}
-	return "", ErrNoAvatarURL
+	return url, nil
 }
 
 type GravatarAvatar struct{}
 
 var UseGravatarAvatar GravatarAvatar
 
-func (_ GravatarAvatar) GetAvatarURL(c *client) (string, error) {
-	if userId, ok := c.userData["user_id"]; ok {
-		if userIdStr, ok := userId.(string); ok {
-			return fmt.Sprintf("//www.gravatar.com/avatar/%s", userIdStr), nil
-		}
-	}
-	return "", ErrNoAvatarURL
+func (_ GravatarAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	return fmt.Sprintf("//www.gravatar.com/avatar/%s", u.UniqueID()), nil
 }
 
 type FileSystemAvatar struct{}
 
 var UseFileSystemAvatar FileSystemAvatar
 
-func (_ FileSystemAvatar) GetAvatarURL(c *client) (string, error) {
-	if userId, ok := c.userData["user_id"]; ok {
-		if userIdStr, ok := userId.(string); ok {
-			if files, err := ioutil.ReadDir("avatars"); err == nil {
-				for _, file := range files {
-					if file.IsDir() {
-						continue
-					}
-					if match, _ := filepath.Match(userIdStr+"*", file.Name()); match {
-						return "/avatars/" + file.Name(), nil
-					}
-				}
+func (_ FileSystemAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	if files, err := ioutil.ReadDir("avatars"); err == nil {
+		for _, file := range files {
+			if file.IsDir() {
+				continue
+			}
+			if match, _ := filepath.Match(u.UniqueID()+"*", file.Name()); match {
+				return "/avatars/" + file.Name(), nil
 			}
 		}
 	}
