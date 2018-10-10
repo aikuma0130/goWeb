4deaf1ce9c5f0d62ab3930e9c6dc5ac10dfbd5d9
Dakie <ktaka0130@gmail.com>
2018-10-10 20:12:53 +0900

    Use text file to read transforms words

--- tools/sprinkle/main.go
+++ tools/sprinkle/main.go
@@ -11,24 +11,24 @@ import (
 
 const otherWord = "*"
 
-var transforms = []string{
-	otherWord,
-	otherWord,
-	otherWord,
-	otherWord,
-	otherWord + "app",
-	otherWord + "site",
-	otherWord + "time",
-	"get" + otherWord,
-	"go" + otherWord,
-	"lets" + otherWord,
-}
+var transforms = make([]string, 0)
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
+	f, err := os.Open("./transforms.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	t := bufio.NewScanner(f)
+	for t.Scan() {
+		transforms = append(transforms, t.Text())
+	}
+
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
-		t := transforms[rand.Intn(len(transforms))]
-		fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
+		transformedWord := transforms[rand.Intn(len(transforms))]
+		fmt.Println(strings.Replace(transformedWord, otherWord, s.Text(), -1))
 	}
 }
