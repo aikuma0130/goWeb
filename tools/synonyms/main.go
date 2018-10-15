feca49328798b24921852cd3869dfa4bd6beeeb5
Dakie <ktaka0130@gmail.com>
2018-10-15 21:27:32 +0900

    Add tools synonyms

---
+++ tools/synonyms/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aikuma0130/goWeb/tools/thesaurus"
+)
+
+func main() {
+	apiKey := os.Getenv("BHT_APIKEY")
+	thesaurus := &thesaurus.BigHuge{APIKey: apiKey}
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		word := s.Text()
+		syns, err := thesaurus.Synonyms(word)
+		if err != nil {
+			log.Fatalf("%qの類語検索に失敗しました: %v\n", word, err)
+		}
+		if len(syns) == 0 {
+			log.Fatalf("%q に類語はありませんでした \n")
+		}
+		for _, syn := range syns {
+			fmt.Println(syn)
+		}
+	}
+}
