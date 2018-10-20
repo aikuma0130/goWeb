c6c797551126af140617b834740f9e06308de92a
Dakie <ktaka0130@gmail.com>
2018-10-20 18:32:55 +0900

    Add twitter connection func

---
+++ distributedSystem/twittervotes/twitter.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"time"
+	"net"
+)
+
+var conn net.Conn
+
+func dial(netw, addr string) net.Conn, error {
+	if conn != nil {
+		conn.Clone()
+		conn = nil
+	}
+
+	netc, err := net.DialTimeout(netw, addr, 5*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	conn = netc
+	return netc, nil
+}
+
+var reader io.ReadCloser
+
+func closeConn() {
+	if conn != nil {
+		conn.Close()
+	}
+	if reader != nil {
+		reader.Close()
+	}
+}
+	
