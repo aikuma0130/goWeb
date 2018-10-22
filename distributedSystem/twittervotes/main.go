b98f91d49e2f7ae5326b727e158a3c8aee7cb310
Dakie <ktaka0130@gmail.com>
2018-10-23 00:04:14 +0900

    Add mongodb dial / close func

--- distributedSystem/twittervotes/main.go
+++ distributedSystem/twittervotes/main.go
@@ -1,3 +1,24 @@
 package main
 
+import (
+	"log"
+
+	mgo "gopkg.in/mgo.v2"
+)
+
 func main() {}
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("MongoDBにダイヤル中: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+func closedb() {
+	db.Close()
+	log.Println("データベース接続が閉じられました")
+
+}
