d0645866e2da3f8e3e570d5d24b08e2813431b8c
Dakie <ktaka0130@gmail.com>
2018-10-30 22:29:24 +0900

    Add nsq handler

--- distributedSystem/twittervotes/counter/main.go
+++ distributedSystem/twittervotes/counter/main.go
@@ -47,4 +47,15 @@ func main() {
 		fatal(err)
 		return
 	}
+	q.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+		vote := string(m.Body)
+		counts[vote]++
+		return nil
+	}))
+
 }
