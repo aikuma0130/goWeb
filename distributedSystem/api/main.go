066e24d185a095f10c4c6ee1d15bc119c01787bb
Dakie <ktaka0130@gmail.com>
2018-11-01 21:59:32 +0900

    Add wrapper function for using api key

--- distributedSystem/api/main.go
+++ distributedSystem/api/main.go
@@ -1,3 +1,19 @@
 package main
 
+import "net/http"
+
 func main() {}
+
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !isValidAPIKey(r.URL.Query().Get("key")) {
+			respondErr(w, r, http.StatusUnauthorized, "不正なAPIキーです")
+			return
+		}
+		fn(w, r)
+	}
+}
+
+func isValidAPIKey(key string) bool {
+	return key == "abc123"
+}
