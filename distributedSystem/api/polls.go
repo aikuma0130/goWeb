9a613309bb578c7821a240d556d4d5610c6052ef
Dakie <ktaka0130@gmail.com>
2018-11-04 22:41:42 +0900

    Add handlePolls for handling /polls request

--- distributedSystem/api/polls.go
+++ distributedSystem/api/polls.go
@@ -8,3 +8,19 @@ type poll struct {
 	Options []string       `json:"options"`
 	Results map[string]int `json:"results,omitempty"`
 }
+
+func handlePolls(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		handlePollsGet(w, r)
+		return
+	case "POST":
+		handlePollsPost(w, r)
+		return
+	case "DELETE":
+		handlePollsDelete(w, r)
+		return
+	}
+	// 未対応のHTTPメソッド
+	respondHTTPErr(w, r, http.StatusNotFound) }
+}
