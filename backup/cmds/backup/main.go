b70aee141ed9b24382d88e74957b9c67553f7b6b
Dakie <ktaka0130@gmail.com>
2018-11-21 23:27:33 +0900

    Add cmdline tool for backup

---
+++ backup/cmds/backup/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"log"
+)
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			flag.PrintDefaults()
+			log.Fatalln(fatalErr)
+		}
+	}()
+	var (
+		dbpath = flag.String("db", "./backupdata", "データベースのディレクトリへのパス")
+	)
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		fatalErr = errors.New("エラー ; コマンドを指定してください")
+		return
+	}
+}
