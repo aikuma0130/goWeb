91bfc2f6998b4a3def502e1be050ab89f7fb00f4
Dakie <ktaka0130@gmail.com>
2018-11-29 20:31:27 +0900

    Add backupd

---
+++ backup/cmds/backupd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			log.Fatalln(fatalErr)
+		}
+	}()
+
+	var (
+		interval = flag.Int("interval", 10, "チェックの間隔(秒単位)")
+		archive  = flag.String("archive", "archive", "アーカイブの保存先")
+		dbpath   = flag.String("db", "./db", "filedbデータベースへのパス")
+	)
+	flag.Parse()
+}
